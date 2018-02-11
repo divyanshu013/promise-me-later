3327a7e9b663cd86789843af6635338ad6515ae4
Divyanshu Maithani <div.blackcat@gmail.com>
2018-02-12 01:10:48 +0530

    :white_check_mark: Add tests

--- test/index.js
+++ test/index.js
@@ -1,24 +1,52 @@
 const test = require('ava');
 const promiseMeLater = require('../src/index');
-let inflight = 0;
-
-const fn = () => new Promise((res, rej) => {
-	console.log('Waiting for promise to resolve');
-	inflight += 1;
-	console.log(inflight);
-	setTimeout(() => {
-		res()
-		console.log('Promise resolved')
-		inflight -= 1;
-		console.log(inflight)
-	}, 3000 + (Math.random() * 100));
-});
-
-const throttled = promiseMeLater(fn, 2, 1000);
 
 test('Limit 1 call per 1000ms', async (t) => {
+	// keep track of inflight requests
+	let inflight = 0;
+	// check at intervals to ensure limit
+	setInterval(() => t.truthy(
+		inflight <= 1,
+		`Inflight requests = ${inflight}`
+	), 1000);
+	const delay = () => new Promise((res) => {
+		inflight += 1;
+		// just in case check while adding inflight requests too
+		t.truthy(inflight <= 1, `Inflight requests = ${inflight}`);
+		setTimeout(() => {
+			res();
+			inflight -= 1;
+		}, 1000 + (Math.random() * 100));
+	});
+	const throttled = promiseMeLater(delay, 1, 1000);
 	const p = [];
 	for (let index = 0; index < 5; index += 1) {
+		p.push(throttled());
+	}
+	await Promise.all(p);
+	t.pass();
+});
+
+test('Limit 3 calls per 1500ms', async (t) => {
+	// keep track of inflight requests
+	let inflight = 0;
+	// check at intervals to ensure limit
+	setInterval(() => t.truthy(
+		inflight <= 3,
+		`Inflight requests = ${inflight}`
+	), 1500);
+	const delay = () => new Promise((res) => {
+		inflight += 1;
+		// just in case check while adding inflight requests too
+		t.truthy(inflight <= 3, `Inflight requests = ${inflight}`);
+		setTimeout(() => {
+			res();
+			inflight -= 1;
+		}, 100 + (Math.random() * 100));
+	});
+	const throttled = promiseMeLater(delay, 3, 1500);
+	const p = [];
+	for (let index = 0; index < 50; index += 1) {
 		p.push(throttled());
 	}
 	await Promise.all(p);
